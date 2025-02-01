0b79f8789ffa803b7d0248bf41e179b1e699bded
Sachin Jakhar <mesachinjakhar@gmail.com>
2025-02-01 15:43:26 +0530

    feat: morgan setup

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 require("dotenv").config;
+const morgan = require("morgan");
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Express middlewares
+app.use(morgan("dev"));
 
 // Route handler
 app.get("/", (req, res) => {
